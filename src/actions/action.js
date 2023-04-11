bb8315747e94f7d415f6628a69892517d14baddd
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-04-11 20:09:11 +0530

    Update action.js

---
+++ src/actions/action.js
@@ -0,0 +1,20 @@
+nes (26 sloc)  444 Bytes
+ 
+
+export const addNumber=()=>{
+    return{
+        type: 'INCREMENT'
+    }
+}
+
+export const subNumber=()=>{
+    return{
+        type: 'DECREMENT'
+    }
+}
+
+export const handlelogin=()=>{
+    return{
+        type: 'SIGN_IN'
+    }
+}
